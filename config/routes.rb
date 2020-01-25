6626a0b092f97a565fbc1476b347c909a146df17
teratti-6 <t.m.t.k.m.s.k.n.k@gmail.com>
2020-01-25 18:05:49 +0900

    initial

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pft#index"
 
+  favicon_link_tag(source='/favicon.ico', options={})
+  
   get 'work', to: 'pft#work'
   get 'contact', to: 'pft#contact'
   get 'detail', to: 'pft#detail'
