fae1de6ee6ab1a78af8ddc8cb8b8de87ffe0c9aa
teratti-6 <t.m.t.k.m.s.k.n.k@gmail.com>
2020-01-25 18:08:35 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pft#index"
-
-  favicon_link_tag(source='/favicon.ico', options={})
   
   get 'work', to: 'pft#work'
   get 'contact', to: 'pft#contact'
